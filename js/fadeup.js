8c6ac83919476d36c23c1799993d17ed6141b439
muramurada <shikitaro1192@gmail.com>
2024-12-11 11:38:11 +0900

    fadeup applied multi block flex items

--- js/fadeup.js
+++ js/fadeup.js
@@ -1,83 +1,60 @@
 function delayScrollAnime() {
+	//flex用ふわっと浮き出るアニメーション効果。flex block全体が同じ動作となる。
+	//flexの列は、任意の数に対応
 	var time = 0.2;//遅延時間の初期値
 	var value = time;//遅延時間を増やす秒数の値保持の変数
-	var elemPos = $(".delayScroll").offset().top;//親要素の位置まで来たら
-	var elemPos2 = $(".delayScroll2").offset().top;//親要素の位置まで来たら
+	//ユーザー設定：HTML内のfadeUpさせたいflex blockの子要素（例：UL）にclassを振る
+	//そのclassをこの配列に記載。
+	var elemPosArray = [".delayScroll", ".delayScroll2"];
+	var elemPos = new Array();
+	var childs = new Array();
+	var numClass = new Array();
+	elemPosArray.forEach(function (ele, i = 1) {
+		elemPos[i] = $(ele).offset().top;
+		childs[i] = $(ele).children();
+		numClass[i] = $(ele).length;
+		i++;
+	});
+	var column = 3;
 
 	var scroll = $(window).scrollTop();//スクロール値を取得
 	var windowHeight = $(window).height();//画面の高さを取得
-	var childs = $(".delayScroll").children();	//子要素を取得
-	var childs2 = $(".delayScroll2").children();	//子要素を取得
-	console.log("scroll=",scroll);
-	console.log("scroll-in=",elemPos - windowHeight);
 
-//memo scrollから何番目のdelayScrollかを判定
-//考え中
+	elemPosArray.forEach(function (ele, ii = 1) {
 
-	if (scroll >= elemPos - windowHeight && !$(".delayScroll").hasClass("play")) {//指定領域内にスクロールが入ったらまた親要素にクラスplayがなければ
-		console.log("Into if");
-		for(var i = 1; i <= childs.length; i++) {//子要素の数だけLoop
-			console.log("i=",i);
-		
-			if (!$(".delayScroll > :nth-child(" + i + ")").hasClass("fadeUp")) {//直下の子要素にアニメーションのクラス名が指定されているかどうかをチェック
-				
-				$(".delayScroll").addClass("play");	//親要素にクラス名playを追加
-				$(".delayScroll > :nth-child(" + i + ")").css("animation-delay", value + "s");//アニメーション遅延のCSS animation-delayを追加し
-				$(".delayScroll > :nth-child(" + i + ")").addClass("fadeUp");//アニメーションのクラス名を追加
-				value = value + time;//delay時間を増加させる
-					
-				// 全ての処理を終わったらplayを外す
-				if(i == childs.length){
-					$(".delayScroll").removeClass("play");
-				}
-			}
-		}
-	}else {
-		$(childs).removeClass("fadeUp");//アニメーションのクラス名を削除
-		value = time;//delay初期値の数値に戻す
-	}
+		if (scroll >= elemPos[ii] - windowHeight && !$(ele).hasClass("play")) {//指定領域内にスクロールが入ったらまた親要素にクラスplayがなければ
+			console.log("value1 =", value);
+			console.log("childs =", childs[ii].length / numClass[ii]);
+			for (var i = 1; i <= childs[ii].length / numClass[ii]; i++) {//子要素の数だけLoop
+				console.log("i=", i);
 
-	if (scroll >= elemPos2 - windowHeight && !$(".delayScroll2").hasClass("play")) {//指定領域内にスクロールが入ったらまた親要素にクラスplayがなければ
-		$(childs).removeClass("fadeUp");//アニメーションのクラス名を削除(a kind of running way)
-		value = time;//delay初期値の数値に戻す
-		for(var i = 1; i <= childs2.length; i++) {//子要素の数だけLoop
-		
-			if (!$(".delayScroll2 > :nth-child(" + i + ")").hasClass("fadeUp")) {//直下の子要素にアニメーションのクラス名が指定されているかどうかをチェック
-				
-					$(".delayScroll2").addClass("play");	//親要素にクラス名playを追加
-					$(".delayScroll2 > :nth-child(" + i + ")").css("animation-delay", value + "s");//アニメーション遅延のCSS animation-delayを追加し
-					$(".delayScroll2 > :nth-child(" + i + ")").addClass("fadeUp");//アニメーションのクラス名を追加
+				if (!$(ele + " > :nth-child(" + i + ")").hasClass("fadeUp")) {//直下の子要素にアニメーションのクラス名が指定されているかどうかをチェック
+
+					$(ele).addClass("play");	//親要素にクラス名playを追加
+					$(ele + " > :nth-child(" + i + ")").css("animation-delay", value + "s");//アニメーション遅延のCSS animation-delayを追加し
+					$(ele + " > :nth-child(" + i + ")").addClass("fadeUp");//アニメーションのクラス名を追加
 					value = value + time;//delay時間を増加させる
-					
+					console.log("value1 if=", value);
+
 					// 全ての処理を終わったらplayを外す
-					if(i == childs2.length){
-						$(".delayScroll2").removeClass("play");
+					if (i == childs[ii].length / numClass[ii]) {
+						$(ele).removeClass("play");
 					}
 				}
 			}
-	}else {
-		$(childs2).removeClass("fadeUp");//アニメーションのクラス名を削除
-		value = time;//delay初期値の数値に戻す
-	}
-}
-
-// 画面をスクロールをしたら動かしたい場合の記述(a kind of running way)
-var scroll = 0;
-	$(window).on('scroll', function (){
-		if($(this).scrollTop() < scroll ){
-			//上スクロールの時の処理
-		}else{
-			//下スクロールの時の処理
-			delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
+		} else {
+			$(childs[ii]).removeClass("fadeUp");//アニメーションのクラス名を削除
+			value = time;//delay初期値の数値に戻す
+			console.log("value else=", value);
 		}
-		scroll = $(this).scrollTop();
-	});// ここまで画面をスクロールをしたら動かしたい場合の記述
-
-	// $(window).scroll(function (){
-	// 		delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
-	// });// ここまで画面をスクロールをしたら動かしたい場合の記述
+		i++;
+	});
+}
+$(window).scroll(function () {
+	delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
 
 // 画面が読み込まれたらすぐに動かしたい場合の記述
-	$(window).on('load', function(){
-		delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
-	});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
